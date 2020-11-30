fbb2d4e6a22fc8c86f87bc29a3ebe840922e355e
Berta VR <bverges@cifpfbmoll.eu>
2020-11-30 16:44:15 +0100

    Merge remote-tracking branch 'origin/pages' into github_pages

--- Menú/Estilo/Scrapping/src/programaScrapping.py
+++ Menú/Estilo/Scrapping/src/programaScrapping.py
@@ -1,9 +1,9 @@
 from urllib.request import urlopen
-url = "http://olympus.realpython.org/profiles/poseidon"
+url = "https://bertavr.github.io/Proyecto_Rick_y_Morty/"
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
-principio = html.find("Name:")
-final = html.find ("<", principio)
-Nombre = html[principio+6 : final]
-print(Nombre)
+principio = html.find("pack>")
+final = html.find ("<")
+nombre = html[principio + 1 : final]
+print (nombre)
