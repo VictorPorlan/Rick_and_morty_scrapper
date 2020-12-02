59c3e3ee1c808eb7b3303d9431c06960b7da5f1d
VictorPorlan <vporlan@cifpfbmoll.eu>
2020-12-02 23:58:42 +0100

    pruebas python

--- src/scrapping.py
+++ src/scrapping.py
@@ -1,14 +1,21 @@
-from src.crawling import localizar_links, conseguir_links, links
+import crawling
 from urllib.request import urlopen
+
+url = "https://bertavr.github.io/Proyecto_Rick_y_Morty/basic.html"
+page = urlopen(url)
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
 
 def localizar_paquetes(html):
         pack = {}
-        inicio_pack = html.find('<div class="pack">')+17
+        inicio_pack = html.find('nombre')
         inicio_nombre = html.find('>', inicio_pack)
         final_nombre = html.find('<', inicio_nombre)
         nombre = html[inicio_nombre+1:final_nombre]
         pack['name'] = nombre
-        return pack
+        return inicio_pack
+
+print(localizar_paquetes(html))
 
 def convertir_link_string(enlace):
         page = urlopen(enlace)
