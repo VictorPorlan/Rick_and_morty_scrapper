157d9a1795664c70a98971d4e7dc6c14c1c44908
VictorPorlan <vporlan@cifpfbmoll.eu>
2020-12-04 20:45:33 +0100

    a

--- src/scrapping.py
+++ src/scrapping.py
@@ -5,26 +5,24 @@ page = urlopen(url)
 html_bytes = page.read()
 html_link = html_bytes.decode("utf-8")
 
-
-
 def crear_caracteristicas(html):
         
-        caracteristicas = {}
+        listado = {}
         final_caracteristicas = html.find('</ul>')
         inicio_caracteristica = html.find('caracteristica')
-        
+        print (final_caracteristicas, inicio_caracteristica)
         while final_caracteristicas > inicio_caracteristica:
                 inicio_caracteristica = html.find('caracteristica')
                 inicio_nombre = html.find('>',inicio_caracteristica)
-                final_nombre = html.find(':',inicio_caracteristica)
+                final_nombre = html.find(':',inicio_nombre)
                 nombre = html[inicio_nombre+1: final_nombre]
                 final_caracteristica = html.find('<',inicio_nombre)
                 caracteristica = html[final_nombre+2: final_caracteristica]
-                caracteristicas[nombre] = caracteristica
-                html= html[final_caracteristica:]
+                listado[nombre] = caracteristica
+                html= html[final_caracteristicas:]
                 final_caracteristicas = html.find('</ul>')
                 inicio_caracteristica = html.find('caracteristica')
-        return caracteristicas, final_caracteristica
+        return listado, final_caracteristicas
 
 def crear_objeto(html):
         objeto = {}
@@ -35,9 +33,9 @@ def crear_objeto(html):
         nombre_pack = html[marca_inicial_nombre +1:marca_final_nombre]
         objeto['nombre'] = nombre_pack
         html = html[marca_final_nombre:]
-        caracteristicas, final_caracteristica = crear_caracteristicas(html)
+        caracteristicas, final_caracteristicas = crear_caracteristicas(html)
         objeto['caracteristicas'] = caracteristicas
-        return objeto, final_caracteristica
+        return objeto, final_caracteristicas
 
 def crear_paquetes(html):
         pack = {}
@@ -46,7 +44,6 @@ def crear_paquetes(html):
         marca_final_nombre = html.find('<', marca_inicial_nombre)
         nombre = html[marca_inicial_nombre+1:marca_final_nombre]
         pack['name'] = nombre
-
         inicio_dimensiones = html.find('altura')
         marca_inicial_altura = html.find(':',inicio_dimensiones)
         marca_final_altura = html.find('<',inicio_dimensiones)
@@ -58,14 +55,14 @@ def crear_paquetes(html):
         pack['dimensiones'] = {'altura':altura, 'ancho':ancho}
         html = html[marca_final_ancho:]
         final_pack = html.find('</div>')
-        objeto, final_caracteristica = crear_objeto(html)
-        html = html[final_caracteristica:]
+        objeto, final_caracteristicas = crear_objeto(html)
+        html = html[final_caracteristicas:]
         pack['objetos']=[objeto]
-        while final_pack > final_caracteristica:
-                objeto, final_caracteristica = crear_objeto(html)
-                html = html[final_caracteristica:]
-                pack['objetos']=[objeto]
-                print (pack)
+        while final_caracteristicas < final_pack:
+                objeto, final_caracteristicas = crear_objeto(html)
+                pack['objetos'].append(objeto)
+                break
         return pack
 
+pos = html_link.find('lista')
 print (crear_paquetes(html_link))
