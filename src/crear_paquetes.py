a677fb5015019e00c40af3b1108e9cb72fe07eed
Berta VR <bverges@cifpfbmoll.eu>
2020-12-05 21:45:06 +0100

    refactor:

--- src/crear_paquetes.py
+++ src/crear_paquetes.py
@@ -1,19 +1,26 @@
 from urllib.request import urlopen
 from crear_objeto import crear_objeto
+from crear_caracteristicas import bucle_crawler
 url = "https://bertavr.github.io/Proyecto_Rick_y_Morty/basic.html"
 page = urlopen(url)
 html_bytes = page.read()
 html_link = html_bytes.decode("utf-8")
 
-def crear_paquetes(html):
-        pack = {}
+
+pack = {}
+#### Por el momento he estado intentando al menos dividir la función, no sé si rula porque no a mí no me deja
+# hacer los prints si hay imports no resueltos.Pruébalo tú, porfa. No sé qué pasa con los import.
+##### Tranquilo! Si no funciona, podemos intentarlo con un closure o si eso tampoco funciona
+#si quitas los dos def que hay de más y lo que te marco con comentario, se queda como antes.
+def crear_paquetes_pack(html):
         inicio_pack = html.find('nombre')
         marca_inicial_nombre = html.find('>', inicio_pack)
         marca_final_nombre = html.find('<', marca_inicial_nombre)
         nombre = html[marca_inicial_nombre+1:marca_final_nombre]
 
         pack['name'] = nombre
-
+        return pack #esto habría que borrarlo si no funciona
+def crear_paquetes_altura(html):
         inicio_dimensiones = html.find('altura')
         marca_inicial_altura = html.find(':',inicio_dimensiones)
         marca_final_altura = html.find('<',inicio_dimensiones)
@@ -24,7 +31,10 @@ def crear_paquetes(html):
         ancho = html[marca_inicial_ancho+2 : marca_final_ancho]
 
         pack['dimensiones'] = {'altura':altura, 'ancho':ancho}
-        
+        return pack,marca_final_ancho #esto habría que borrarlo si no funciona
+def integrar_paquete(html):
+        inicio_ancho = html.find('ancho') #esto también porque está en la función anterior (sería repetirse)
+        marca_final_ancho = html.find('<',inicio_ancho)#esto también porque está en la función anterior (sería repetirse)
         html = html[marca_final_ancho:]
         carac, html, nombre_pack = crear_objeto(html)
         final_pack = html.find('/div')
@@ -37,4 +47,5 @@ def crear_paquetes(html):
                 final_pack = html.find('/div')
                 if final_caracteristicas == -1:
                         break
-        return pack,html
+        return pack,html 
+        print (integrar_paquete(html_link))
