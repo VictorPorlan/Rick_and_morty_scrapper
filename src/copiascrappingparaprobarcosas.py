d1821f743824dfc1edea0fcccbfa09732b25080f
Berta VR <bverges@cifpfbmoll.eu>
2020-12-05 18:56:07 +0100

    refactor:

--- src/copiascrappingparaprobarcosas.py
+++ src/copiascrappingparaprobarcosas.py
@@ -6,17 +6,16 @@ html_bytes = page.read()
 html_link = html_bytes.decode("utf-8")
 
 def crear_caracteristicas(html):
-    carcteriticas_dicc = {}
-    final_caracteristicas = html.find('final')
-    inicio_caracteristica = html.find('caracteristica')
-
-        caracteristica_añadir = False
-        for marca in html:
-                if marca == '"caracterica">':
-                    caracteristica_añadir = True
-                elif marca == "</li>":
-                    caracteristica_añadir = False
-                if caracteristica_añadir== True:
-                    carcteriticas_dicc[nombre] = marca
+    caracteristicas_dicc= {}
+    caracteristica_añadir = False
+    for marca in html:
+        if marca == '"caracterstica">':
+            caracteristica_añadir = True
+        elif marca == "</li>":
+            caracteristica_añadir = False
+        if caracteristica_añadir== True:
+            carcteriticas_dicc[nombre] = marca
         return caracteristicas_dicc
+        print (crear_caracteristicas(html_link))
+
     
