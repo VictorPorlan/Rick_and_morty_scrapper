937eeed041b6dbf417bceba4a4d48b6a92e43345
Berta VR <bverges@cifpfbmoll.eu>
2020-12-07 16:17:45 +0100

    refactor: :bug:

--- src/probando que el crawling recorra todo.py
+++ src/probando que el crawling recorra todo.py
@@ -5,7 +5,7 @@ link_index_pagina = "https://bertavr.github.io/Proyecto_Rick_y_Morty/index.html"
 
 def sacar_html(link_index): 
 #esto es lo que saca html, lo he puesto en función para poder usarlo en el crawler y en el bucle sin estar copiando cada vez
-    page = urlopen(index)
+    page = urlopen(link_index)
     html_bytes = page.read()
     html_index = html_bytes.decode("utf-8")
     assert isinstance(html_index, str)
@@ -17,11 +17,12 @@ def conseguir_links(link_index):
     lista_links = [link_index]
     for pagina in lista_links:
         html_pagina = sacar_html(pagina)
-        linksRegex = re.compile(r'a href=\'|\"(.*?)\'|\">')
-        mo_links = links_regex.findall(html_pagina)
-        for link in mo links if link in lista_links or mo_links.count(link) > 1:
+        linksRegex = re.compile(r'a href=(\'|\")(.*?)\.\S(\'|\")>')
+        mo_links = linksRegex.findall(html_pagina)
+    for link in mo_links:
+        if link in lista_links or mo_links.count(link) > 1:
             mo_links.remove(link)
-        lista_links = lista_links + mo_links
+    lista_links = lista_links + mo_links
     assert isinstance(lista_links, list)
     return lista_links
 
