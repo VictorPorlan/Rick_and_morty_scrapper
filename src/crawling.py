1f26a057021a5f1d8def160210d19500f0bb1e5d
Berta VR <bverges@cifpfbmoll.eu>
2020-12-07 18:33:02 +0100

    perf: :zap: el crawler pasa por todos los links

--- src/crawling.py
+++ src/crawling.py
@@ -5,7 +5,18 @@ page = urlopen(url)
 html_bytes = page.read()
 html_index = html_bytes.decode("utf-8")
 
+
+link_index_pagina = "https://bertavr.github.io/Proyecto_Rick_y_Morty/index.html"
+
+def sacar_html(index): 
+#esto es lo que saca html, lo he puesto en función para poder usarlo en el crawler y en el bucle sin estar copiando cada vez
+    page = urlopen(index)
+    html_bytes = page.read()
+    html_index = html_bytes.decode("utf-8")
+    assert isinstance(html_index, str)
+    return html_index
 assert isinstance(html_index, str)
+#aquí hay que hacer un try pq podría fallar a la hora de sacar la página (y no depende de nuestro código)
 
 def localizar_links(html_index):
     start_link= html_index.find('a href=')
@@ -14,7 +25,12 @@ def localizar_links(html_index):
     url = html_index[start_quote+1 : end_quote]
     return url, end_quote
 
-def conseguir_links(html):
+def unir_lista(por_crawlear , links_pagina_nueva):
+    for elemento in links_pagina_nueva:
+        if elemento not in por_crawlear:
+            por_crawlear.append(elemento)
+
+def links_una_pagina(html):
     links = []
     while True:
         url, endpos = localizar_links(html)
@@ -28,6 +44,14 @@ def conseguir_links(html):
     assert isinstance(links, list)
     assert len(links) == html.count('https')
             
-        
-        
+def links_todo(pagina_raiz):
+    por_crawlear = [pagina_raiz]
+    crawleadas = []
+    while por_crawlear:
+        pagina = por_crawlear.pop()
+        if pagina not in crawled:
+            unir_lista(por_crawlear, links_una_pagina(sacar_html(pagina))
+            crawleadas.append(pagina)
+    return crawleadas
+    print links_todo(link_index_pagina)
             
