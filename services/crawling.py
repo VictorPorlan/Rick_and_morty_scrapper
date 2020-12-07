c78e217549cdfbbb05703f703ebc2163f56a6d82
VictorPorlan <vporlan@cifpfbmoll.eu>
2020-12-07 20:26:30 +0100

    a

--- services/crawling.py
+++ services/crawling.py
@@ -1,14 +1,18 @@
 from urllib.request import urlopen
 seed= 'https://bertavr.github.io/Proyecto_Rick_y_Morty/index.html'
-def convertir_string(link):
-    page = urlopen(link)
+page = urlopen(seed)
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+def convertir_string(pagina):
+    page = urlopen(pagina)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
     return html
-    assert isinstance(html, str)
 
 def localizar_links(html_index):
     start_link= html_index.find('a href=')
+    if start_link == -1:
+        return None, 0
     start_quote= html_index.find("'", start_link)
     end_quote= html_index.find("'", start_quote + 1)
     url = html_index[start_quote+1 : end_quote]
@@ -30,16 +34,17 @@ def conseguir_links(html):
         else:
             break
     return links
+
 def crawl_web (seed):
     tocrawl =[seed]
     crawled =[]
     while tocrawl:
-        page = tocrawl.pop()
-        if page not in crawled:
-            tocrawl = por_crawlear(tocrawl, conseguir_links(convertir_string(page)))
-            crawled.append(page)
+        pagina = tocrawl.pop()
+        if pagina not in crawled:
+            tocrawl = por_crawlear(tocrawl, conseguir_links(convertir_string(pagina)))
+            crawled.append(pagina)
     return crawled
-print(crawl_web(seed))
+print(crawl_web(html))
         
         
             
