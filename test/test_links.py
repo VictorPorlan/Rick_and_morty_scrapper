cee53c400d535b42f85381505ea1d7b8275e77ee
VictorPorlan <vporlan@cifpfbmoll.eu>
2020-12-08 02:58:04 +0100

    pytest da problemas

--- test/test_links.py
+++ test/test_links.py
@@ -1,3 +1,3 @@
-from src.crawling import conseguir_links, html_index, localizar_links
+from services.crawl_web import crawl_web
 def test_links():
-    assert conseguir_links(html_index) == ['https://bertavr.github.io/Proyecto_Rick_y_Morty/index.html', 'https://bertavr.github.io/Proyecto_Rick_y_Morty/premium.html', 'https://bertavr.github.io/Proyecto_Rick_y_Morty/standard.html', 'https://bertavr.github.io/Proyecto_Rick_y_Morty/basic.html']
+    assert crawl_web('https://bertavr.github.io/Proyecto_Rick_y_Morty/premium.html') == ['https://bertavr.github.io/Proyecto_Rick_y_Morty/premium.html', 'https://bertavr.github.io/Proyecto_Rick_y_Morty/basic.html', 'https://bertavr.github.io/Proyecto_Rick_y_Morty/standard.html', 'https://bertavr.github.io/Proyecto_Rick_y_Morty/index.html']
--- test/test_objetos.py
+++ test/test_objetos.py
@@ -1,2 +1,4 @@
-from src.scrapping import crear_objeto, html, crear_caracteristicas
+from services.crear_calidad import crear_calidad
+def test_calidad():
+    assert crear_calidad("<li class='calidad'>Basic</li>") == ('Basic',25)
 
